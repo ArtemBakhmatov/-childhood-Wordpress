9fc8344e692f8b5e43e76cfb994e625cf1b32a96
Artem <ip.bakhmatov@mail.ru>
2022-12-23 22:23:26 +0300

    Динамический вывод постов и картинки-превью

--- functions.php
+++ functions.php
@@ -12,5 +12,6 @@
         wp_enqueue_script('childhood-scripts', get_template_directory_uri() . '/assets/js/main.min.js', array(), null, true);
     };
 
-    add_theme_support('custom-logo'); // Подключение кастомоного логотипа
+    add_theme_support('custom-logo');       // Подключение кастомоного логотипа
+    add_theme_support('post-thumbnails');   // Позволяет устанавливать миниатюру посту.(Записи -> Изоб-е записи)
 ?>
