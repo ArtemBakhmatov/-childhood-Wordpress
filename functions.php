041d490c1b449b783850d9cde404f03f94471a2c
Artem <ip.bakhmatov@mail.ru>
2022-12-28 19:31:14 +0300

    кнопка активности в меню при переходе

--- functions.php
+++ functions.php
@@ -25,6 +25,10 @@
             if($item->current) {
                 $atts['class'] .= ' header__nav-item-active';
             }
+            //print_r($item);
+            if( $item->ID === 152 && ( in_category( 'soft_toys' ) || in_category( 'edu_toys' ))){
+                $atts['class'] .= ' header__nav-item-active';
+            }
         };
         return $atts;
     }
